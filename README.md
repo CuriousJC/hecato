2d8419fc9661434caa161266b31a564ab7b8e459
Justin Crosby <justin.crosby@gmail.com>
2024-07-15 19:22:52 -0400

    initial hello world commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hecato
+
+hecato - Short for Hecatoncheires, the Hundred-Handed giants of Greek mythology is intended to be a file system cli for doing various file system operations.
+
+https://en.wikipedia.org/wiki/Hecatoncheires
---
+++ cmd/hecato/main.go
@@ -0,0 +1,22 @@
+/*
+
+main executable for hecato
+
+go run c:/repos/hecato/cmd/hecato/main.go
+go build c:/repos/hecato/cmd/hecato/main.go
+
+*/
+
+//TODO: make the name of the executable 'hecato'
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("-----------------")
+	fmt.Println("hello world from hecato!")
+
+}
