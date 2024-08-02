d532700ecc04786e4a3522d86907dba44aefc5cf
Justin Crosby <justin.crosby@gmail.com>
2024-08-02 17:05:05 -0400

    adding an examples override flag

--- cmd/hecato/main.go
+++ cmd/hecato/main.go
@@ -6,6 +6,8 @@ go run c:/repos/hecato/cmd/hecato/main.go
 go build -o hecato.exe c:/repos/hecato/cmd/hecato/main.go
 c:/repos/hecato/hecato.exe
 
+go run c:/repos/hecato/cmd/hecato/main.go -method largefiles -hits 10 -target 'c:/windows' -hide=false
+
 git tag v1.0.0
 git push origin v1.0.0
 git push origin main --tags
@@ -31,8 +33,13 @@ func main() {
 	target := flag.String("target", "undefined", "target of the given method")
 	hits := flag.String("hits", "15", "How many hits for given method")
 	hide := flag.Bool("hide", true, "Hide access errors from output")
+	examples := flag.Bool("examples", false, "Show examples of usage")
 
 	flag.Parse()
+
+	if *examples {
+		*method = "examples"
+	}
 
 	//everything begins with the method
 	//TODO: last changed files, last created files, file contents search, directory size summary
@@ -55,8 +62,8 @@ func main() {
 		}
 	case "examples":
 		fmt.Println("----Examples of usage-----")
-		fmt.Println("go run c:/repos/hecato/cmd/hecato/main.go -method largefiles -hits 25 -target d:/SteamLibrary")
-		fmt.Println("go run c:/repos/hecato/cmd/hecato/main.go -method largefiles -hits 10 -target 'c:/windows' -hide=false")
+		fmt.Println("hecato -method largefiles -hits 25 -target d:/SteamLibrary")
+		fmt.Println("hecato -method largefiles -hits 10 -target 'c:/windows' -hide=false")
 
 	default:
 		fmt.Println("That wasn't an option for our 'm' method")
